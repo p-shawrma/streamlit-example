133077e596569980311e3775f5349a070f15a95d
p-shawrma <157201518+p-shawrma@users.noreply.github.com>
2024-07-29 15:08:02 +0530

    Update telematics_summary.py

--- telematics_summary.py
+++ telematics_summary.py
@@ -4,6 +4,7 @@ import pandas as pd
 from streamlit_echarts import st_echarts
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import clickhouse_connect
 
@@ -25,7 +26,7 @@ client = clickhouse_connect.get_client(
 # Set page configuration to wide mode and set page title
 st.set_page_config(layout="wide", page_title="Vehicle Telematics Dashboard")
 
-# Set Mapbox access token
+# Mapbox Access Token
 px.set_mapbox_access_token("pk.eyJ1IjoicC1zaGFybWEiLCJhIjoiY2xzNjRzbTY1MXNodjJsbXUwcG0wNG50ciJ9.v32bwq-wi6whz9zkn6ecow")
 
 # Function to connect to ClickHouse and get data
@@ -127,103 +128,113 @@ def main():
     df = df_main  # Use df for data from calculated_main_telematics table
     df2 = df_tel  # Use df2 for data from calculated_telematics_soc
     df3 = df_cohort  # Use df3 for cohorting data
-
+    
+    # Sidebar filters
     with st.sidebar:
         st.markdown("### Filters")
         df_main['date'] = pd.to_datetime(df_main['date'], errors='coerce')
-
-        max_date = df_main['date'].max().date()
-        min_date = df_main['date'].min().date()
-        start_date_last_7_days = max_date - timedelta(days=6)
-
+        
+        # Calculate the maximum and minimum dates in the dataset
+        max_date = df_main['date'].max()
+        min_date = df_main['date'].min()
+        
+        # Calculate the start date for the last 7 days range
+        start_date_last_7_days = max_date - pd.Timedelta(days=6)
+        
         date_range = st.date_input(
             'Select Date Range', 
-            value=[start_date_last_7_days, max_date],
-            min_value=min_date,
-            max_value=max_date
+            value=[start_date_last_7_days, max_date],  # Sets default range to last 7 days
+            min_value=min_date,  # The earliest date a user can select
+            max_value=max_date   # The latest date a user can select
         )
-
+        
         df_filtered = df_main
         df_filtered_tel = df_tel
         df_filtered_cohort = df_cohort
         df_filtered_mapping = df_mapping
-
+        
         if len(date_range) == 2 and date_range[0] and date_range[1]:
-            start_date = pd.Timestamp(date_range[0])
-            end_date = pd.Timestamp(date_range[1])
-
-            df_filtered = df_filtered[(df_filtered['date'] >= start_date) & (df_filtered['date'] <= end_date)]
+            df_filtered = df_filtered[(df_filtered['date'] >= pd.Timestamp(date_range[0])) & (df_filtered['date'] <= pd.Timestamp(date_range[1]))]
             df_filtered_tel['end_date'] = pd.to_datetime(df_filtered_tel['end_date'])
             df_filtered_cohort['date'] = pd.to_datetime(df_filtered_cohort['date'])
-            df_filtered_tel = df_filtered_tel[(df_filtered_tel['end_date'] >= start_date) & (df_filtered_tel['end_date'] <= end_date)]
-            df_filtered_cohort = df_filtered_cohort[(df_filtered_cohort['date'] >= start_date) & (df_filtered_cohort['date'] <= end_date)]
-
+            df_filtered_tel = df_filtered_tel[(df_filtered_tel['end_date'] >= pd.Timestamp(date_range[0])) & (df_filtered_tel['end_date'] <= pd.Timestamp(date_range[1]))]
+            df_filtered_cohort = df_filtered_cohort[(df_filtered_cohort['date'] >= pd.Timestamp(date_range[0])) & (df_filtered_cohort['date'] <= pd.Timestamp(date_range[1]))]
+            
             # Customer Name filter
-            partner_ids = df_filtered['partner_id'].dropna().unique().tolist()
+            partner_ids = df_filtered_mapping['client_name'].dropna().unique().tolist()
             selected_partner_ids = st.multiselect('Customer Name', partner_ids)
 
-            # Filter dataframe by customer name if selected
             if selected_partner_ids:
                 df_filtered_mapping = df_filtered_mapping[df_filtered_mapping['client_name'].isin(selected_partner_ids)]
+                filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
+                df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
 
             # Product filter
-            products = df_filtered['product'].dropna().unique().tolist()
+            products = df_filtered_mapping['battery_type'].dropna().unique().tolist()
             selected_products = st.multiselect('Product', products)
 
-            # Filter dataframe by product if selected
             if selected_products:
                 df_filtered_mapping = df_filtered_mapping[df_filtered_mapping['battery_type'].isin(selected_products)]
+                filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
+                df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
 
             # Registration Number filter
-            reg_nos = df_filtered['reg_no'].dropna().unique().tolist()
+            reg_nos = df_filtered_mapping['reg_no'].dropna().unique().tolist()
             selected_reg_nos = st.multiselect('Registration Number', reg_nos)
 
-            # Filter dataframe by registration number if selected
             if selected_reg_nos:
                 df_filtered_mapping = df_filtered_mapping[df_filtered_mapping['reg_no'].isin(selected_reg_nos)]
+                filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
+                df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
 
             # Chassis Number filter
-            chassis_nos = df_filtered['chassis_number'].dropna().unique().tolist()
+            chassis_nos = df_filtered_mapping['chassis_number'].dropna().unique().tolist()
             selected_chassis_nos = st.multiselect('Chassis Number', chassis_nos)
 
-            # Filter dataframe by chassis number if selected
             if selected_chassis_nos:
                 df_filtered_mapping = df_filtered_mapping[df_filtered_mapping['chassis_number'].isin(selected_chassis_nos)]
+                filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
+                df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
 
             # City filter
-            cities = df_filtered['deployed_city'].dropna().unique().tolist()
+            cities = df_filtered_mapping['location'].dropna().unique().tolist()
             selected_cities = st.multiselect('City', cities)
 
-            # Filter dataframe by city if selected
             if selected_cities:
                 df_filtered_mapping = df_filtered_mapping[df_filtered_mapping['location'].isin(selected_cities)]
+                filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
+                df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
+                df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
 
-            # Extract filtered telematics numbers
-            filtered_telematics_numbers = df_filtered_mapping['telematics_number'].unique().tolist()
-
-            # Filter main dataframes by telematics number
-            df_filtered = df_filtered[df_filtered['telematics_number'].isin(filtered_telematics_numbers)]
-            df_filtered_tel = df_filtered_tel[df_filtered_tel['telematics_number'].isin(filtered_telematics_numbers)]
-            df_filtered_cohort = df_filtered_cohort[df_filtered_cohort['telematics_number'].isin(filtered_telematics_numbers)]
-
+            # Update the duration slider based on filtered data
             if not df_filtered.empty:
                 min_km, max_km = df_filtered['total_km_travelled'].agg(['min', 'max'])
                 min_km, max_km = int(min_km), int(max_km)
             else:
-                min_km, max_km = 0, 0
+                min_km, max_km = 0, 0  # Defaults when no data is available
 
             initial_min_km = max(10, min_km)
-
             km_range = st.slider("Select daily distance travelled range(kms)", min_km, max_km, (initial_min_km, max_km))
-
             df_filtered = df_filtered[(df_filtered['total_km_travelled'] >= km_range[0]) & (df_filtered['total_km_travelled'] <= km_range[1])]
 
-            st.markdown("### Cache Management")
-            if st.button("Clear Cache"):
-                st.cache_data.clear()
+        st.markdown("### Cache Management")
+        if st.button("Clear Cache"):
+            st.cache_data.clear()
+            st.experimental_rerun()
 
+    # Layout: 3 columns
     col1, col2, col3 = st.columns(3)
 
+    # Column 1 - Average Distance Travelled
     with col1:
         st.markdown("## Average Distance")
 
@@ -231,7 +242,6 @@ def main():
             grouped_distance = df_filtered.groupby('date')['total_km_travelled']
             avg_dist_all_vehicles_per_day = df_filtered.groupby('date')['total_km_travelled'].mean().round(1).reset_index()
             overall_avg_dist_per_day = avg_dist_all_vehicles_per_day['total_km_travelled'].mean().round(1)
-
             st.metric(" ", f"{overall_avg_dist_per_day:.2f} km")
 
             boxplot_data = {
@@ -301,14 +311,16 @@ def main():
             }
             st_echarts(options=stacked_column_options, height="400px")
         else:
-            st.write("Please select a date range and other filters to view analytics.")
+            st.write("Please select a date range and other filters to view analytics.") 
 
+    # Column 2 - Average Range of the Fleet
     with col2:
         st.markdown("## Average Range")
+    
         df_range = df_filtered[(df_filtered['total_km_travelled'] >= 0) & (df_filtered['total_discharge_soc'] < 0)]
         avg_range_fleet = np.sum(df_range['total_km_travelled']) * -100 / np.sum(df_range['total_discharge_soc'])
         st.metric(" ", f"{avg_range_fleet:.2f} km")
-
+    
         if not df_range.empty:
             grouped = df_range.groupby('date')['predicted_range']
             boxplot_data = {
@@ -345,43 +357,45 @@ def main():
             avg_slow_charge_soc = df_filtered['slow_charge_soc'].mean()
             avg_fast_charge_sessions = df_filtered['fast_charge_count'].mean()
             avg_fast_charge_soc = df_filtered['fast_charge_soc'].mean()
-
+            
             st.metric("Average Slow Charge Sessions in a day", f"{avg_slow_charge_sessions:.2f}")
             st.metric("Average Slow Charge SOC in a day", f"{avg_slow_charge_soc:.2f}")
             st.metric("Average Fast Charge Sessions in a day", f"{avg_fast_charge_sessions:.2f}")
             st.metric("Average Fast Charge SOC in a day", f"{avg_fast_charge_soc:.2f}")
         else:
             st.write("Please select a date range and other filters to view analytics.")
-
+    
     with col3:
         st.markdown("## Distance travelled, Range and Runtime")
+            
         df_range = df_filtered[(df_filtered['total_km_travelled'] >= 0)]
-
+            
         if not df_range.empty:
-            invalid_reg_no_values = [None, np.nan, "NA", "0", "FALSE", "NULL", "false", "False"]
-            invalid_telematics_values = [None, np.nan, "111111111111111", "FALSE", "11111111111111", "A", "false", "False"]
-
+            invalid_reg_no_values = [None, np.nan, "NA", "0", "FALSE", "NULL","false","False"]
+            invalid_telematics_values = [None, np.nan, "111111111111111", "FALSE", "11111111111111", "A","false","False"]
+            
             df_range['chassis_number'] = replace_invalid_values(df_range['chassis_number'], 'Unknown Chassis', [None, np.nan, False, 0, '0'])
             df_range['reg_no'] = replace_invalid_values(df_range['reg_no'], 'Unknown Reg', invalid_reg_no_values)
             df_range['telematics_number'] = replace_invalid_values(df_range['telematics_number'], 'Unknown Telematics', invalid_telematics_values)
-
+            
             df_grouped = df_range.groupby(['chassis_number', 'reg_no', 'telematics_number'], dropna=False).agg(
                 total_km_travelled_sum=('total_km_travelled', 'sum'),
                 total_discharge_soc_sum=('total_discharge_soc', 'sum'),
                 total_runtime_minutes_sum=('total_runtime_minutes', 'sum')
             ).reset_index()
-
+            
             df_grouped['Range'] = df_grouped['total_km_travelled_sum'] * (-100) / df_grouped['total_discharge_soc_sum']
+            
             avg_run_time = df_range['total_runtime_minutes'].median()
             st.markdown(f"#### Average Run Time: {avg_run_time:.2f} minutes")
-
+            
             avg_run_time_per_day = df_range.groupby(['chassis_number', 'reg_no', 'telematics_number'], dropna=False)['total_runtime_minutes'].median().reset_index(name='avg_run_time_per_day')
             df_grouped = df_grouped.merge(avg_run_time_per_day, on=['chassis_number', 'reg_no', 'telematics_number'])
-
+            
             df_grouped['chassis_number'].replace('Unknown Chassis', None, inplace=True)
             df_grouped['reg_no'].replace('Unknown Reg', None, inplace=True)
             df_grouped['telematics_number'].replace('Unknown Telematics', None, inplace=True)
-
+            
             df_display = df_grouped[['chassis_number', 'reg_no', 'telematics_number', 'total_km_travelled_sum', 'total_runtime_minutes_sum', 'avg_run_time_per_day', 'Range']].rename(
                 columns={
                     'chassis_number': 'Chassis Number',
@@ -392,27 +406,27 @@ def main():
                     'avg_run_time_per_day': 'Average Run Time Per Day'
                 }
             )
-
+            
             st.dataframe(df_display, height=400)
         
         st.markdown("## Vehicle level Average Slow and Fast Charge SOC")
-        if not df_filtered.empty:
-            df_charging = df_filtered.groupby('reg_no').agg(
-                average_slow_charge_soc=('slow_charge_soc', 'mean'),
-                average_fast_charge_soc=('fast_charge_soc', 'mean')
-            ).reset_index().round(1)
 
-            df_charging.rename(columns={
-                'reg_no': 'Registration Number',
-                'average_slow_charge_soc': 'Average Slow Charge SOC',
-                'average_fast_charge_soc': 'Average Fast Charge SOC'
-            }, inplace=True)
+        df_charging = df_filtered.groupby('reg_no').agg(
+            average_slow_charge_soc=('slow_charge_soc', 'mean'),
+            average_fast_charge_soc=('fast_charge_soc', 'mean')
+        ).reset_index().round(1)
 
-            st.dataframe(df_charging, height=300)
+        df_charging.rename(columns={
+            'reg_no': 'Registration Number',
+            'average_slow_charge_soc': 'Average Slow Charge SOC',
+            'average_fast_charge_soc': 'Average Fast Charge SOC'
+        }, inplace=True)
 
+        st.dataframe(df_charging, height=300)     
+        
     df_charging_locations = df_filtered_tel[(df_filtered_tel['change_in_soc'] > 0) & (df_filtered_tel['soc_type'] == "Charging")]
 
-    if not df_filtered_cohort.empty:
+    if not df_filtered_cohort.empty:                
         st.markdown("## Distance Travelled distribution ")
         pivot_table = df_filtered_cohort.pivot_table(
             index='distance_bucket',
@@ -422,18 +436,20 @@ def main():
             fill_value=0
         )
         pivot_table.columns = pivot_table.columns.strftime('%d-%m-%Y')
+        
         pivot_table_percentage = pivot_table.div(pivot_table.sum(axis=0), axis='columns') * 100
         formatted_pivot_table_percentage = pivot_table_percentage.style.format("{:.1f}%")
         st.table(formatted_pivot_table_percentage)
+
     else:
         st.write("Please select a date range and other filters to view analytics.")
-
+        
     if not df_filtered.empty:
         st.markdown("## Day Wise Data")
         st.dataframe(df_filtered, height=300)
     else:
         st.write("df_filtered is empty")
-
+        
     if not df_filtered_tel.empty:
         st.markdown("## SOC Data")
         st.dataframe(df_filtered_tel, height=300)
